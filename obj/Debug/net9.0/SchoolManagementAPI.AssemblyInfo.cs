3dd1764642f5b31c01e5c7bdf7d7c9a1fcfa674b
IMBEKA <imbeka06@gmail.com>
2025-07-17 15:49:39 +0300

    second

--- obj/Debug/net9.0/SchoolManagementAPI.AssemblyInfo.cs
+++ obj/Debug/net9.0/SchoolManagementAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SchoolManagementAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b28f3497be32a9507cb4ef4500c5ba082dede29e")]
 [assembly: System.Reflection.AssemblyProductAttribute("SchoolManagementAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SchoolManagementAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
